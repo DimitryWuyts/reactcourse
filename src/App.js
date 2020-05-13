6c0df3b2fd3793d13c8fe044fb0b09194a2fe18f
Dimitry Wuyts <dimiw1337@hotmail.com>
2020-05-13 20:14:27 +0200

    added navbar component before i forget

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import MainContent from "./MainContent";
+import NavBar from "./NavBar";
 
 function App() {
     return (<div>
-
         <Header/>
+        <NavBar/>
         <MainContent/>
         <Footer/>
             </div>
-
     )
 
 }
-
-
 export default App
---
+++ src/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavBar() {
+    return(
+        <nav>
+            <h5>here my navbar comes</h5>
+        </nav>
+    )
+
+}
+export default NavBar
